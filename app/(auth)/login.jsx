c7aa589fbd93833598a1f69947bf8a1590c054a1
K-0bby <k.hoenyefia@gmail.com>
2025-04-27 13:07:56 +0000

    update frontend and added backend with appwrite

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,52 +1,77 @@
-import { Pressable, StyleSheet, Text } from 'react-native'
-import { Link } from 'expo-router'
-import { Colors } from '../../constants/colors'
+import {
+  StyleSheet,
+  Text,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { Link } from "expo-router";
+import { Colors } from "../../constants/colors";
+import { useState } from "react";
 
-import ThemedView from '../../components/themedview'
-import ThemedText from '../../components/themedtext'
-import Spacer from '../../components/spacer'
-import ThemedButton from '../../components/themedbutton'
+import ThemedView from "../../components/themedview";
+import ThemedText from "../../components/themedtext";
+import Spacer from "../../components/spacer";
+import ThemedButton from "../../components/themedbutton";
+import ThemedTextInput from "../../components/themedtextInput";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = async () => {
-    console.log('login form submitted')
-  }
+    console.log("login form submitted", email, password);
+  };
 
   return (
-    <ThemedView style={styles.container}>
-      
-      <Spacer />
-      <ThemedText title={true} style={styles.title}>
-        Login to Your Account
-      </ThemedText>
-
-      <ThemedButton onPress={handleSubmit}>
-        <Text style={{ color: '#f2f2f2' }}>Login</Text>
-      </ThemedButton>
-
-      <Spacer height={100} />
-      <Link href="/register" replace>
-        <ThemedText style={{ textAlign: "center" }}>
-          Register instead
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <ThemedView style={styles.container}>
+        <Spacer />
+        <ThemedText title={true} style={styles.title}>
+          Login to Your Account
         </ThemedText>
-      </Link>
 
-    </ThemedView>
-  )
-}
+        <ThemedTextInput
+          style={{ width: "80%", marginBottom: 20 }}
+          placeholder="Email"
+          keyboardType="email-address"
+          onChangeText={setEmail}
+          value={email}
+        />
 
-export default Login
+        <ThemedTextInput
+          style={{ width: "80%", marginBottom: 20 }}
+          placeholder="Password"
+          onChangeText={setPassword}
+          value={password}
+          secureTextEntry
+        />
+
+        <ThemedButton onPress={handleSubmit}>
+          <Text style={{ color: "#f2f2f2" }}>Login</Text>
+        </ThemedButton>
+
+        <Spacer height={100} />
+        <Link href="/register" replace>
+          <ThemedText style={{ textAlign: "center" }}>
+            Register instead
+          </ThemedText>
+        </Link>
+      </ThemedView>
+    </TouchableWithoutFeedback>
+  );
+};
+
+export default Login;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
   title: {
     textAlign: "center",
     fontSize: 18,
-    marginBottom: 30
-  }
-})
+    marginBottom: 30,
+  },
+});
--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,51 +1,74 @@
-import { StyleSheet, Text } from 'react-native'
-import { Link } from 'expo-router'
+import {
+  Keyboard,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { Link } from "expo-router";
+import { useState } from "react";
 
-import ThemedView from '../../components/themedview'
-import ThemedText from '../../components/themedtext'
-import Spacer from '../../components/spacer'
-import ThemedButton from '../../components/themedbutton'
+import ThemedView from "../../components/themedview";
+import ThemedText from "../../components/themedtext";
+import Spacer from "../../components/spacer";
+import ThemedButton from "../../components/themedbutton";
+import ThemedTextInput from "../../components/themedtextInput";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = async () => {
-    console.log('register form submitted')
-  }
+    console.log("register form submitted", email, password);
+  };
 
   return (
-    <ThemedView style={styles.container}>
-
-      <Spacer />
-      <ThemedText title={true} style={styles.title}>
-        Register an Account
-      </ThemedText>
-
-      <ThemedButton onPress={handleSubmit}>
-        <Text style={{ color: '#f2f2f2' }}>Register</Text>
-      </ThemedButton>
-
-      <Spacer height={100} />
-      <Link href="/login" replace>
-        <ThemedText style={{ textAlign: "center" }}>
-          Login instead
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <ThemedView style={styles.container}>
+        <Spacer />
+        <ThemedText title={true} style={styles.title}>
+          Register an Account
         </ThemedText>
-      </Link>
 
-    </ThemedView>
-  )
-}
+        <ThemedTextInput
+          style={{ width: "80%", marginBottom: 20 }}
+          placeholder="Email"
+          keyboardType="email-address"
+          onChangeText={setEmail}
+          value={email}
+        />
 
-export default Register
+        <ThemedTextInput
+          style={{ width: "80%", marginBottom: 20 }}
+          placeholder="Password"
+          onChangeText={setPassword}
+          value={password}
+          secureTextEntry
+        />
+
+        <ThemedButton onPress={handleSubmit}>
+          <Text style={{ color: "#f2f2f2" }}>Register</Text>
+        </ThemedButton>
+
+        <Spacer height={100} />
+        <Link href="/login" replace>
+          <ThemedText style={{ textAlign: "center" }}>Login instead</ThemedText>
+        </Link>
+      </ThemedView>
+    </TouchableWithoutFeedback>
+  );
+};
+
+export default Register;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
   title: {
     textAlign: "center",
     fontSize: 18,
-    marginBottom: 30
+    marginBottom: 30,
   },
-})
+});
